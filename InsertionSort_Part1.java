e2d755c7fd03b025832bf1c758ae4ea51d899f7e
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2018-12-20 22:36:46 -0700

    Update InsertionSort_Part1.java

--- InsertionSort_Part1.java
+++ InsertionSort_Part1.java
@@ -1,16 +1,23 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insertion_sorting;
 
-public class Solution {
+/**
+ *
+ * @author teva
+ */
+public class Insertion_sorting {
 
-    // Complete the insertionSort1 function below.
-    static void insertionSort1(int n, int[] arr) {
+   // Complete the insertionSort1 function below.
+    static void insertionSort1(int n, int[] arr) 
+    {
+        if(n > arr.length) return;
+        if(arr.length < 1)return;
         int temp = arr[n-1];
+        boolean check = false;
         for(int i = arr.length - 2; i >= 0; i-- )
         {
             if(temp < arr[i])
@@ -20,10 +27,18 @@ public class Solution {
             }
             else
             {
-                arr[i + 1] = temp;
+                arr[i + 1] = temp;               
                 print(arr);
+                check =true;
+                break;
             }
         }
+        if(!check)
+        {
+            arr[0] = temp;
+            print(arr);
+        }
+        
     }
     static void print(int[] arr)
     {
@@ -33,25 +48,15 @@ public class Solution {
         }
         System.out.print("\n");
     }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
-
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int n = 10;
+        int[] arr = new int[]{2 ,3 ,4 ,5, 6, 7, 8, 9, 10, 1};
         insertionSort1(n, arr);
-
-        scanner.close();
     }
+    
 }
