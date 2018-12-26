d2cee35cf6824545747ca8512033fa411990c9c0
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2018-12-25 19:50:50 -0700

    Update Bubble_Sorting.java

--- Bubble_Sorting.java
+++ Bubble_Sorting.java
@@ -23,6 +23,22 @@ public class Bubble_Sorting {
         }     
     }
     
+   public static void bubbleSortingLittleOptimized(int[] arr)
+    {
+        for(int j = 0; j < arr.length - 1; j++)
+        {
+           for(int i = 0; i < arr.length - j - 1; i++)
+            {
+                if(arr[i] > arr[i+1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }   
+        }     
+    }
+    
     public static void printArray(int[] a)
     {
         for(int i=0; i < a.length; i++)
